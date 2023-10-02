553e48560c32b6bce72c3927ad0313f93a58297b
FuzzyCat444 <38776317+FuzzyCat444@users.noreply.github.com>
2023-10-02 15:47:47 -0400

    Add files via upload

--- src/Vector.h
+++ src/Vector.h
@@ -2,8 +2,8 @@
 #define VECTOR_H
 
 // Vector structure with no operators/functions, just to group x and y coords.
-// Function calls have overhead when used in a tight loop, so we inline allocate
-// vector operations.
+// Function calls have overhead when used in a tight loop, so we inline vector
+// operations for a speed boost.
 typedef struct Vector {
     double x;
     double y;
